8dd133ae045e0bb4b92f73942487420aaa94e6e3
mariblan <mariablancocanal@gmail.com>
2026-01-14 13:00:55 +0100

    Added tailwind styling

--- 02_Web_APIS/lectures/src/main.js
+++ 02_Web_APIS/lectures/src/main.js
@@ -16,17 +16,19 @@ heading.style.fontSize = '24px';
 // Creating a li element with a delete button
 function addLiItem(value) {
   const li = document.createElement('li');
+  li.className = 'w-50 text-xs flex justify-between items-center bg-stone-200 mb-2 p-2 rounded';
 
   const span = document.createElement('span');
   span.textContent = value;
-
+  span.className = 'text-gray-800 font-medium';
   const deleteBtn = document.createElement('button');
   deleteBtn.textContent = 'Delete';
+  deleteBtn.className = 'bg-red-900 text-white px-2 py-1 rounded hover:bg-red-700';
   deleteBtn.addEventListener('click', (e) => {
     li.remove();
   });
 
-  li.classList.add("listItem");
+  li.classList.add('listItem');
   li.appendChild(span);
   li.appendChild(deleteBtn);
   list.appendChild(li);
@@ -41,27 +43,32 @@ form.addEventListener('submit', (e) => {
 
 // Creating a card element
 const cardUser = (user) => {
-    const card = document.createElement("div");
+  const card = document.createElement('div');
+  card.className = 'flex flex-col gap-1 bg-gray-100 w-56 text-sm rounded-xl shadow';
 
-    const image = document.createElement("img");
-    image.src = user.image
-    image.alt = user.name
+  const image = document.createElement('img');
+  image.src = 'https://picsum.photos/200';
+  image.alt = user.name;
+  image.className = 'max-h-34 rounded-t-xl';
 
-    const cardBody = document.createElement("div");
-    const title = document.createElement("h2")
-    title.textContent = user.name
-    
-    const email = document.createElement('p');
-    email.textContent = `Email: ${user.email}`
+  const cardBody = document.createElement('div');
+  cardBody.className = 'p-4';
 
-    cardBody.appendChild(title);
-    cardBody.appendChild(email);
+  const title = document.createElement('h2');
+  title.textContent = user.name;
+  title.classList.add('font-bold');
 
-    card.appendChild(image);
-    card.appendChild(cardBody);
+  const email = document.createElement('p');
+  email.textContent = `Email: ${user.email}`;
 
-    userList.appendChild(card);
-}
+  cardBody.appendChild(title);
+  cardBody.appendChild(email);
+
+  card.appendChild(image);
+  card.appendChild(cardBody);
+
+  userList.appendChild(card);
+};
 
 // FETCH -> then/catch logic
 // fetch('https://jsonplaceholder.typicode.com/users')
@@ -78,23 +85,20 @@ const cardUser = (user) => {
 // }
 
 // FETCH -> async/await logic
-const getUsers = async() => {
-    try{
-        const res = await fetch('https://jsonplaceholder.typicode.com/users');
-        const data = await res.json()
-        console.log(data)
-        return data;
-    }catch(err){
-        console.error('Error: ', err);
-    }
-}
-
+const getUsers = async () => {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.error('Error: ', err);
+  }
+};
 
 // displaying the data retrive by the getUsers() async function
-window.addEventListener("load", async () => {
-    const users = await getUsers();
-    console.log(users);
-    users.map((user) => cardUser(user));
-})
-
-
+window.addEventListener('load', async () => {
+  const users = await getUsers();
+  console.log(users);
+  users.map((user) => cardUser(user));
+});
